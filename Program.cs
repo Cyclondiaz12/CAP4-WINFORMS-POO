bbc4bc423a6dd6687f7ba583456f280022867fef
Cyclondiaz12 <119273326+Cyclondiaz12@users.noreply.github.com>
2024-03-07 20:29:04 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+
+using System;
 using System.Windows.Forms;
 
 namespace Ejercicios4
 {
-    internal static class Program
+    public partial class caldera : Form
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private CalculadoraCaldera logica;
+
+        public caldera()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            InitializeComponent();
+            logica = new CalculadoraCaldera();
+            logica.TemperaturaCambiada += Logica_TemperaturaCambiada;
+            ActualizarTemperatura();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Temperatura aceptable
+        {
+            logica.AjustarTemperatura(true);
+            MessageBox.Show("Gracias por usar el programa");
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Temperatura no aceptable
+        {
+            logica.AjustarTemperatura(false);
+        }
+
+        private void Logica_TemperaturaCambiada(object sender, EventArgs e)
+        {
+            ActualizarTemperatura();
+        }
+
+        private void ActualizarTemperatura()
+        {
+            int temperatura = logica.ObtenerTemperaturaActual();
+            textBox1.Text = temperatura.ToString() + "°C";
+
+            // Habilitar o deshabilitar los botones según la temperatura actual
+            button1.Enabled = temperatura >= CalculadoraCaldera.TemperaturaMinimaAceptable;
+            button2.Enabled = true; // Opcional: siempre permitir ajustar la temperatura hacia abajo
         }
     }
 }
